3951563943db65a5d334ace37f5b3eda99471b78
AndreaPane <pane.and94@gmail.com>
2025-02-24 13:38:06 +0100

    implementata autenticazione fortify

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,52 @@
+<x-layout>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Registrati</h1>
+            </div>
+        </div>
+    </div> 
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-12"></div>
+            
+            <form method="POST" action="{{route('register')}}">
+                @csrf
+                
+                
+                
+                <div class="mb-3">
+                    
+                    <label for="username" class="form-label">User Name</label>
+                    <input type="text" name="name" class="form-control" id="username">
+                    
+                </div>
+                
+                <div class="mb-3">
+                    
+                    <label for="email" class="form-label">Email address</label>
+                    <input type="email" name="email" class="form-control" id="email" >
+                    
+                </div>
+                <div class="form-group mt-3">
+                    <label for="password">Password</label>
+                    <input id="password" type="password" class="form-control" name="password" required>
+                </div>
+                <div class="form-group mt-3">
+                    <label for="password_confirmation">Conferma Password</label>
+                    <input id="password_confirmation" type="password" class="form-control"
+                    name="password_confirmation" required>
+                </div>
+                
+                <button type="submit" class="btn btn-danger">Submit</button>
+            </form>
+        </div>
+    </div>
+</x-layout>
+
+
+
+
+
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
 
-<h1>puzzzo di merda</h1>
+<h1>ciao</h1>
 
 </x-layout>
