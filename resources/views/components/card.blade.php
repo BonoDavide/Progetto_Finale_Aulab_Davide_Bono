a5a4f60ed92c2e1928b9fad060f840cb9bfa8752
Davide Bono <davidebono1605@gmail.com>
2025-03-03 18:18:59 +0100

    ristrutturata navbar, modificata palette e colore testi, aggiunto dropdown utente

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -3,10 +3,10 @@
         class="card-img-top" alt="Immagine dell'articolo {{ $post->title }}">
 
     <div class="card-body">
-        <h2 class="h5 card-title text-black">{{ $post->title }}</h2>
-        <h5 class="card-title text-black">{{ $post->price }}€</h5>
+        <h2 class="h5 card-title text-white">{{ $post->title }}</h2>
+        <h5 class="card-title text-white">{{ $post->price }}€</h5>
         <h5 class="card-title">{{ $post->category->name }}</h5>
-        <p class="card-title text-black">{{ $post->description }}</p>
+        <p class="card-title text-white">{{ $post->description }}</p>
         <a href="{{ route('post.detailPost', compact('post')) }}" class="btn btn-primary">Dettaglio prodotto</a>
     </div>
 </div>
--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
-<footer class="bg-body-tertiary navCustom text-center">
+<footer class="navCustom text-center">
     <!-- Grid container -->
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="row justify-content-center text-white">
             <div class="col-6 pt-5 mb-3 text-center">
                 <h5>{{ __('ui.become') }}</h5>
                 <p>{{ __('ui.pBecome') }}</p>
@@ -16,35 +16,35 @@
         <section class="mb-4">
             <!-- Facebook -->
             <a data-mdb-ripple-init class="btn text-white btn-floating m-1" style="background-color: #3b5998;"
-                href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+                href="{{ route('homePage') }}" role="button"><i class="fab fa-facebook-f"></i></a>
 
             <!-- Twitter -->
             <a data-mdb-ripple-init class="btn text-white btn-floating m-1" style="background-color: #55acee;"
-                href="#!" role="button"><i class="fab fa-twitter"></i></a>
+                href="{{ route('homePage') }}" role="button"><i class="fab fa-twitter"></i></a>
 
             <!-- Google -->
             <a data-mdb-ripple-init class="btn text-white btn-floating m-1" style="background-color: #dd4b39;"
-                href="#!" role="button"><i class="fab fa-google"></i></a>
+                href="{{ route('homePage') }}" role="button"><i class="fab fa-google"></i></a>
 
             <!-- Instagram -->
             <a data-mdb-ripple-init class="btn text-white btn-floating m-1" style="background-color: #ac2bac;"
-                href="#!" role="button"><i class="fab fa-instagram"></i></a>
+                href="{{ route('homePage') }}" role="button"><i class="fab fa-instagram"></i></a>
 
             <!-- Linkedin -->
             <a data-mdb-ripple-init class="btn text-white btn-floating m-1" style="background-color: #0082ca;"
-                href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
+                href="{{ route('homePage') }}" role="button"><i class="fab fa-linkedin-in"></i></a>
             <!-- Github -->
             <a data-mdb-ripple-init class="btn text-white btn-floating m-1" style="background-color: #333333;"
-                href="#!" role="button"><i class="fab fa-github"></i></a>
+                href="{{ route('homePage') }}" role="button"><i class="fab fa-github"></i></a>
         </section>
         <!-- Section: Social media -->
     </div>
     <!-- Grid container -->
 
     <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
-        © 2025 Copyright:
-        <a class="text-body" href="https://mdbootstrap.com/">404 BrainNotFound SNC</a>
+    <div class="text-center p-3 text-white" style="background-color: rgba(0, 0, 0, 0.05);">
+        © 2025 Copyright: 404 BrainNotFound SNC
+        {{-- <a class="text-body" href="https://mdbootstrap.com/">404 BrainNotFound SNC</a> --}}
     </div>
     <!-- Copyright -->
 </footer>
