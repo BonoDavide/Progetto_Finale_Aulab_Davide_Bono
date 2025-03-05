02a51ef76db957a21f8ababdf45b1cceab0c6b69
Lorenzo Pattaro <lorenzopattaro@live.it>
2025-03-05 16:52:07 +0100

    User Story 8 test

--- app/Livewire/CreatePost.php
+++ app/Livewire/CreatePost.php
@@ -10,6 +10,7 @@ use Livewire\WithFileUploads;
 use Livewire\Attributes\Validate;
 use App\Jobs\GoogleVisionLabelImage;
 use App\Jobs\GoogleVisionSafeSearch;
+use App\Jobs\RemoveFaces;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 // #[Validate("required", message: "Inserisci il titolo del film")]
@@ -48,12 +49,14 @@ class CreatePost extends Component
             foreach($this->images as $image){
                 $newFileName = "posts/{$this->post->id}";
                 $newImage = $this->post->images()->create(['path' => $image->store($newFileName, 'public')]);
-                dispatch(new ResizeImage($newImage->path, 900, 900));
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVisionLabelImage($newImage->id));
-                // $this->post->images()->create([
-                //     'path' => $image->store('images', 'public')
-                // ]);
+                // dispatch(new ResizeImage($newImage->path, 900, 900));
+                // dispatch(new GoogleVisionSafeSearch($newImage->id));
+                // dispatch(new GoogleVisionLabelImage($newImage->id));
+                RemoveFaces::withChain([
+                    new ResizeImage($newImage->path, 900, 900),
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVisionLabelImage($newImage->id)
+                ])->dispatch($newImage->id);
             }
 
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
